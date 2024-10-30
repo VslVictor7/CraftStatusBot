2a3faab0e5f78626ab1fa8ac76efc87d7ed9cff3
VslVictor7 <vic.collfac358@outlook.com>
2024-10-29 23:23:12 -0300

    refactor: making comments for stale files

--- run_ip_updater.vbs
+++ run_ip_updater.vbs
@@ -1,4 +1,5 @@
 Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run """D:\Minecraft Servers\Pegar IP\script.bat""", 0, False
+WshShell.Run """C:\Users\NomeDoUsuario\Desktop\script.bat""", 0, False
 
-
+' Mudaro diretorio dentro do .Run para a path do script.bat, exemplo abaixo:
+' WshShell.Run """C:\Users\NomeDoUsuario\Desktop\script.bat""", 0, False
--- script.bat
+++ script.bat
@@ -1,2 +1,5 @@
 @echo off
-"F:\Programas\python 3.11.9\python.exe" "D:\Minecraft Servers\Pegar IP\ip_updater.py" > "D:\Minecraft Servers\Pegar IP\log.txt" 2>&1
+"path de onde o python está instalado" "path para o arquivo python ip_updater.py" > "path para o log.txt" 2>&1
+
+:: Exemplo para usar em cima:
+::"F:\Programas\python 3.11.9\python.exe" "D:\Minecraft Servers\Pegar IP\ip_updater.py" > "D:\Minecraft Servers\Pegar IP\log.txt" 2>&1
