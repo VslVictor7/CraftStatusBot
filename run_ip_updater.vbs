09b273d3da94a1aab03de40125bd6855879133cf
VslVictor7 <vic.collfac358@outlook.com>
2024-10-30 15:10:06 -0300

    chore: move files for organization

--- run_ip_updater.vbs
+++
@@ -1,5 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run """C:\Users\NomeDoUsuario\Desktop\script.bat""", 0, False
-
-' Mudaro diretorio dentro do .Run para a path do script.bat, exemplo abaixo:
-' WshShell.Run """C:\Users\NomeDoUsuario\Desktop\script.bat""", 0, False
--- script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"path de onde o python está instalado" "path para o arquivo python ip_updater.py" > "path para o log.txt" 2>&1
-
-:: Exemplo para usar em cima:
-::"F:\Programas\python 3.11.9\python.exe" "D:\Minecraft Servers\Pegar IP\ip_updater.py" > "D:\Minecraft Servers\Pegar IP\log.txt" 2>&1
