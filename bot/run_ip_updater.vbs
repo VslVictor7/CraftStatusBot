42c986ee13ccfefe87c2530b6bc3435b5f5dc847
VslVictor7 <vic.collfac358@outlook.com>
2024-10-30 15:17:25 -0300

    remove: check last commit

--- bot/run_ip_updater.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run """D:\Minecraft Servers\Pegar IP\script.bat""", 0, False
--- bot/script.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"F:\Programas\python 3.11.9\python.exe" "D:\Minecraft Servers\Pegar IP\ip_updater.py" > "D:\Minecraft Servers\Pegar IP\log.txt" 2>&1
