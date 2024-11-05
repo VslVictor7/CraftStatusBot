2ab6bb958ef8a472e9f77f107d5253e99a84a709
VslVictor7 <vic.collfac358@outlook.com>
2024-11-05 20:02:39 -0300

    feat: main.py for better readability

---
+++ bot/main.py
@@ -0,0 +1,29 @@
+from minecraft import ip_updater
+import discord, os, aiohttp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+MESSAGE_ID = int(os.getenv('MESSAGE_ID'))
+
+bot = ip_updater.MyBot()
+
+@bot.event
+async def on_ready():
+    async with aiohttp.ClientSession() as session:
+        channel = bot.get_channel(CHANNEL_ID)
+        if channel:
+            try:
+                message = await channel.fetch_message(MESSAGE_ID)
+                print("Bot pronto para monitoramento de IP, Servidor e Jogadores.")
+                await ip_updater.update_message_periodically(channel, message, session)
+            except discord.DiscordException as e:
+                print(f"Erro ao buscar mensagem: {e}")
+                await bot.close()
+        else:
+            print("Canal não detectado.")
+            await bot.close()
+
+bot.run(TOKEN)
