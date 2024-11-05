b370ea232546a842747faf4a8711ae8b2093e7ce
VslVictor7 <vic.collfac358@outlook.com>
2024-11-05 20:03:40 -0300

    chore: changes for code

--- script.bat
+++ script.bat
@@ -1,2 +1,2 @@
 @echo off
-"F:\Programas\python 3.11.9\python.exe" "D:\Minecraft Servers\Pegar IP\bot\minecraft\ip_updater.py" > "D:\Minecraft Servers\Pegar IP\bot\log.txt" 2>&1
+"F:\Programas\python 3.11.9\python.exe" "D:\Minecraft Servers\Pegar IP\bot\minecraft\ip_updater.py" > "D:\Minecraft Servers\Pegar IP\bot\config\log.txt" 2>&1
