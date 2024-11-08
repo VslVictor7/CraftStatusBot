edba229c46afa3a96f1c05e1d8dede3700d093ab
VslVictor7 <vic.collfac358@outlook.com>
2024-11-07 21:12:20 -0300

    chore: renamed file for better readability

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,4 @@
-from minecraft import ip_updater
+from minecraft import message_manager
 import discord, os, aiohttp
 from dotenv import load_dotenv
 from datetime import datetime
@@ -9,7 +9,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 MESSAGE_ID = int(os.getenv('MESSAGE_ID'))
 
-bot = ip_updater.MyBot()
+bot = message_manager.MyBot()
 
 # Comandos.
 
@@ -42,7 +42,7 @@ async def on_ready():
             try:
                 message = await channel.fetch_message(MESSAGE_ID)
                 print("Bot pronto para monitoramento de IP, Servidor e Jogadores.")
-                await ip_updater.update_message_periodically(channel, message, session)
+                await message_manager.update_message_periodically(channel, message, session)
             except discord.DiscordException as e:
                 print(f"Erro ao buscar mensagem: {e}")
                 await bot.close()
--- bot/minecraft/bot_sender.py
+++ bot/minecraft/bot_sender.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "token com aspas, é uma STR"  # Insira seu token aqui
-CHANNEL_ID = 'id deve ser sem aspas, é um INT'  # Insira o ID do canal aqui
+TOKEN = "token com aspas, é uma STR"
+CHANNEL_ID = 'id deve ser sem aspas, é um INT'
 
-# Criação do bot com intents
 intents = discord.Intents.default()
-intents.messages = True  # Permite que o bot leia mensagens
+intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -14,10 +13,8 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Bot {bot.user.name} conectado ao Discord!')
 
-    # Obtém o canal pelo ID
     channel = bot.get_channel(CHANNEL_ID)
     if channel is not None:
-        # Envia a mensagem
         await channel.send('Mensagem placeholder para meu IP.')
     else:
         print('Canal não encontrado.')
