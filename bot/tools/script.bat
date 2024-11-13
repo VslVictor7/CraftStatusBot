1d91515f6fe4d4629a3d0fb6d2d683edb08bc4b5
VslVictor7 <vic.collfac358@outlook.com>
2024-11-13 20:22:17 -0300

    chore: python executed in unbuffered mode

--- bot/tools/script.bat
+++ bot/tools/script.bat
@@ -6,5 +6,5 @@ if %errorlevel% equ 0 (
 ) else (
     echo Falha ao ativar o ambiente virtual > "D:\Minecraft Servers\Status_Manager_Bot\bot\env\log.txt"
 )
-python --version >> "D:\Minecraft Servers\Status_Manager_Bot\bot\env\log.txt" 2>&1
-python "D:\Minecraft Servers\Status_Manager_Bot\bot\main.py" >> "D:\Minecraft Servers\Status_Manager_Bot\bot\env\log.txt" 2>&1
+python -u --version >> "D:\Minecraft Servers\Status_Manager_Bot\bot\env\log.txt" 2>&1
+python -u "D:\Minecraft Servers\Status_Manager_Bot\bot\main.py" >> "D:\Minecraft Servers\Status_Manager_Bot\bot\env\log.txt" 2>&1
