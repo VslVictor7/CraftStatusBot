ca0e1bdc2558178392641e60d3f51565c2c1fef6
VslVictor7 <vic.collfac358@outlook.com>
2024-11-27 15:09:29 -0300

    feat: integrate PostgreSQL database to operate independently from the bot

--- bot/core/utils/database.py
+++ bot/core/utils/database.py
@@ -1,17 +1,30 @@
-import sqlite3
+import psycopg2
 import os
 import pytz
 from datetime import datetime
+from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'bot_database.db')
+load_dotenv()
 
-conn = sqlite3.connect(DB_PATH)
+DB_HOST = os.getenv('DB_HOST', 'postgres-db')
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = os.getenv('DB_NAME', 'botdb')
+DB_USER = os.getenv('DB_USER', 'myuser')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'mypassword')
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+)
+
 cursor = conn.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS server_data (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     nome TEXT,
     player_left TEXT,
     players_online INTEGER,
@@ -23,7 +36,13 @@ conn.commit()
 conn.close()
 
 def insert_server_data(player_name, server_online, players_online, player_left=None):
-    conn = sqlite3.connect(DB_PATH)
+    conn = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+    )
     cursor = conn.cursor()
 
     br_tz = pytz.timezone('America/Sao_Paulo')
@@ -31,7 +50,7 @@ def insert_server_data(player_name, server_online, players_online, player_left=N
 
     cursor.execute('''
     INSERT INTO server_data (nome, players_online, server_status, player_left, timestamp)
-    VALUES (?, ?, ?, ?, ?)
+    VALUES (%s, %s, %s, %s, %s)
     ''', (player_name, players_online, 'online' if server_online else 'offline', player_left, current_time))
 
     conn.commit()
