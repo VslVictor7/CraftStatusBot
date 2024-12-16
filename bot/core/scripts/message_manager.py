5fcae2a27e7655375279a970915d66035e9155f5
VslVictor7 <vic.collfac358@outlook.com>
2024-12-16 13:04:37 -0300

    chore: port

--- bot/core/scripts/message_manager.py
+++ bot/core/scripts/message_manager.py
@@ -30,7 +30,7 @@ def create_embed(ip, server_online, players_online, version, player_names):
         title="Status do Servidor Minecraft",
         color=0x00ff00 if server_online else 0xff0000
     )
-    embed.add_field(name="🖥️ IP", value=ip if server_online else "Nenhum", inline=False)
+    embed.add_field(name="🖥️ IP", value=f"{ip}:25565" if server_online else "Nenhum", inline=False)
     embed.add_field(name="📶 Status", value="🟢 Online" if server_online else "🔴 Offline", inline=False)
     embed.add_field(
         name="👥 Jogadores Online",
