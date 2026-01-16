15ea7e600a241a0cfde7a20ff1a064ac7e5f672e
VslVictor7 <vic.collfac358@outlook.com>
2026-01-16 18:55:03 -0300

    refactor: organized main.go separated with init.go

---
+++ cmd/bot/init.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+
+	"discord-bot-go/internal/commands"
+	"discord-bot-go/internal/connections"
+	"discord-bot-go/internal/log"
+	"discord-bot-go/internal/presentation"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func initDiscord(token string) (*discordgo.Session, error) {
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao criar sessão Discord: %w", err)
+	}
+
+	dg.Identify.Intents =
+		discordgo.IntentsGuilds |
+			discordgo.IntentsGuildMessages |
+			discordgo.IntentsMessageContent
+
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.Type != discordgo.InteractionApplicationCommand {
+			return
+		}
+		cmdName := i.ApplicationCommandData().Name
+		cmd, ok := commands.RegisteredCommands[cmdName]
+		if !ok {
+			return
+		}
+		cmd.Handler(s, i)
+	})
+
+	if err := dg.Open(); err != nil {
+		return nil, fmt.Errorf("erro ao abrir conexão Discord: %w", err)
+	}
+
+	for name, cmd := range commands.RegisteredCommands {
+		_, err := dg.ApplicationCommandCreate(dg.State.User.ID, "", cmd.Command())
+		if err != nil {
+			fmt.Printf("Erro ao registrar /%s: %v\n", name, err)
+		}
+	}
+
+	return dg, nil
+}
+
+func initRcon(addr, password string) (*connections.Chat, error) {
+	rcon := &connections.Chat{
+		Addr:     addr,
+		Password: password,
+	}
+
+	if err := rcon.Connect(); err != nil {
+		return nil, fmt.Errorf("erro ao conectar no RCON: %w", err)
+	}
+
+	return rcon, nil
+}
+
+func startMCWatcher(dg *discordgo.Session, eventsChannelID, logPath string) error {
+	mcBridge, err := presentation.NewMinecraftChatBridge(dg, eventsChannelID)
+	if err != nil {
+		return fmt.Errorf("erro ao criar bridge MC: %w", err)
+	}
+
+	watcher := log.New(logPath)
+	watcher.Register(func(line string) {
+		mcBridge.Handle(line)
+	})
+
+	go watcher.Start()
+	return nil
+}
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
+	"discord-bot-go/internal/presentation"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"discord-bot-go/internal/commands"
-	"discord-bot-go/internal/connections"
-	"discord-bot-go/internal/presentation"
-	"discord-bot-go/internal/server"
-
-	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
@@ -20,94 +14,36 @@ func main() {
 	godotenv.Load()
 
 	token := os.Getenv("DISCORD_TOKEN")
-
-	serverAddr := os.Getenv("HOST")
-
-	statusChannelID := os.Getenv("CHANNEL_ID")
-	statusMessageID := os.Getenv("MESSAGE_ID")
-
-	eventsChannelID := os.Getenv("EVENTS_CHANNEL_ID")
-
 	rconAddr := os.Getenv("RCON_ADDR")
 	rconPassword := os.Getenv("RCON_PASSWORD")
+	eventsChannelID := os.Getenv("EVENTS_CHANNEL_ID")
+	logPath := os.Getenv("SERVER_LOG_PATH")
 
-	dg, err := discordgo.New("Bot " + token)
+	dg, err := initDiscord(token)
 	if err != nil {
-		fmt.Println("Erro ao criar sessão:", err)
+		fmt.Println(err)
 		return
 	}
+	defer dg.Close()
 
-	dg.Identify.Intents =
-		discordgo.IntentsGuilds |
-			discordgo.IntentsGuildMessages |
-			discordgo.IntentsMessageContent
-
-	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if i.Type != discordgo.InteractionApplicationCommand {
-			return
-		}
-
-		cmdName := i.ApplicationCommandData().Name
-		cmd, ok := commands.RegisteredCommands[cmdName]
-		if !ok {
-			return
-		}
-
-		cmd.Handler(s, i)
-	})
-
-	if err := dg.Open(); err != nil {
-		fmt.Println("Erro ao abrir conexão:", err)
+	rcon, err := initRcon(rconAddr, rconPassword)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-
-	for name, cmd := range commands.RegisteredCommands {
-		_, err := dg.ApplicationCommandCreate(
-			dg.State.User.ID,
-			"",
-			cmd.Command(),
-		)
-		if err != nil {
-			fmt.Printf("Erro ao registrar /%s: %v\n", name, err)
-		}
-	}
-
-	rconChat := &connections.Chat{
-		Addr:     rconAddr,
-		Password: rconPassword,
-	}
-
-	if err := rconChat.Connect(); err != nil {
-		fmt.Println("Erro ao conectar no RCON:", err)
-		return
-	}
-	defer rconChat.Close()
+	defer rcon.Close()
 
 	chatBridge := &presentation.DiscordChatBridge{
 		Session:   dg,
 		ChannelID: eventsChannelID,
-		RconChat:  rconChat,
+		RconChat:  rcon,
 	}
 	chatBridge.Register()
 
-	monitor := &server.ServerMonitor{
-		ServerAddr: serverAddr,
-		Interval:   2 * time.Second,
-
-		StatusEmbed: &server.StatusEmbed{
-			Session:   dg,
-			ChannelID: statusChannelID,
-			MessageID: statusMessageID,
-			BotName:   dg.State.User.Username,
-		},
-
-		PlayerEvents: &server.PlayerEvents{
-			Session:   dg,
-			ChannelID: eventsChannelID,
-		},
+	if err := startMCWatcher(dg, eventsChannelID, logPath); err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	monitor.Start()
 
 	fmt.Println("Bot rodando")
 
@@ -116,5 +52,4 @@ func main() {
 	<-stop
 
 	fmt.Println("Desligando bot")
-	_ = dg.Close()
 }
